63939af3fd92aee91c2b26154c9a489ac3550d35
minestrenght <you@example.com>
2014-07-17 12:14:57 +0530

    modified code to test case of 2

--- black-box-test-case-range-1-100/Launch.java
+++ black-box-test-case-range-1-100/Launch.java
@@ -3,36 +3,50 @@ import java.util.Scanner;
 public class Launch {
 
     public static void main(String[] args) {
-		int noOfTest = 9;
-		int test = 1;
-		int num;
-		String[] input = new String[noOfTest];
+		int noOfTest = 2;
+		int test = 0;
+		int[] input = new int[noOfTest];
 		String[] expected = new String[noOfTest];
-		String[] actual = new String[noOfTest];
+		int[] actual = new int[noOfTest];
 		String[] status = new String[noOfTest];
 		String[] remarks = new String[noOfTest];
-		String invalidClass = "Invalid Equivalent Class";
-		String validClass = "Valid Equivalent Class";
-		String pass = "Pass";
-		String fail = "Fail";
-		String outOfRange = "Out Of Range";
-		String colFormat = "-%s27";
-		String notAnum = "Not a number";
 		Scanner sc = new Scanner(System.in);
+        boolean isOut;
 		do {
+            isOut = false;
 			System.out.printf("Enter +ve no. (1 - 100): ");
-			try {
-				input[test] = sc.nextLine();
-				num = Integer.parseInt(input[test]);
-				actual[test] = Integer.toString(num * num);
-			} catch (NumberFormatException ex) {
-				expected[test] = notAnum;
-				actual[test] = notAnum;
-				remarks[test] = notAnum;
-				status[test] = fail;
-			}
-		} while(noOfTest >= test ++);
+            input[test] = sc.nextInt();
+            if(0 >= input[test] || 101 <= input[test]) {
+                remarks[test] = "Invalid Equivalent Class";
+                isOut = true;
+            } else {
+                remarks[test] = "Valid Equivalent Class";
+            }
+            actual[test] = input[test] * input[test];
+            System.out.printf("Enter expected square of %d : ", input[test]);
+            expected[test] = Integer.toString(sc.nextInt());
+            if(isOut) {
+                expected[test] = "Out Of Range";
+            }
+            if(Integer.toString(actual[test]).equals(expected[test])) {
+                status[test] = "Pass";
+            } else {
+                status[test] = "Fail";
+            }
+		} while(noOfTest > ++ test);
 		sc.close();
 		sc = null;
+        System.out.println();
+        String line = String.format("%79s", ' ').replace(" ", "-");
+        String fmt = "|%-10s|%-6s|%-25s|%-13s|%-10s|%-8s|\n";
+        System.out.println(line);
+        System.out.printf(fmt, "Test Case", "Input", "Remarks", "Expected", "Actual", "Status");
+        System.out.println(line);
+        System.out.println(line);
+        test = 0;
+        do {
+            System.out.printf(fmt, (test + 1), input[test], remarks[test], expected[test], actual[test], status[test]);
+            System.out.println(line);
+        } while(noOfTest > ++ test);
     }
 }
